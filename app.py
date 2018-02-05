3c25fe415437f90a0edb98a74e6a69094a12879b
Ilya Zhidkov <zidkov.ilja@gmail.com>
2018-02-06 00:47:01 +0100

    Added real-time bidirectional communication between client and server.

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@socketio.on('message')
+def handleMessage(msg):
+    emit('response', msg)
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    socketio.run(app)
